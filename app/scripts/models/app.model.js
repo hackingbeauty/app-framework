658d25f5bd8695c495e7b4b46f05279681e5b8eb
Mark Muskardin <mark.muskardin@gmail.com>
2014-06-06 21:50:23 -0700

    first commit

---
+++ app/scripts/models/app.model.js
@@ -0,0 +1,31 @@
+/*
+ * app.model.js
+ * Model module
+*/
+
+/*jslint         browser : true, continue : true,
+  devel  : true, indent  : 2,    maxerr   : 50,
+  newcap : true, nomen   : true, plusplus : true,
+  regexp : true, sloppy  : true, vars     : false,
+  white  : true
+*/
+/*global TAFFY, $, app */
+
+app.model = (function () {
+  'use strict';
+  var
+    configMap = { },
+    stateMap  = { },
+
+  isFakeData = false,
+
+  initModule;
+
+  initModule = function () {
+    // initialize anonymous person
+  };
+
+  return {
+    initModule : initModule
+  };
+}());
